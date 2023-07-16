80bdd3ec5ac287d3db1427a63a070164a3e7ac3f
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2023-07-16 21:57:08 +0530

    Created the main file stracture

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../../assets/images/logo.png'
+
+const Header = () => {
+  return (
+    <header className='header flex items-center'>
+        <div className="container">
+          <div className="flex items-center justify-between">
+            {/*-----------Logo--------------*/}
+            <div>
+              <img src={logo} alt="" />
+            </div>
+
+          </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
